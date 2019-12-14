ded38b580a2caf1748eb33fecd3fde9f9e43285f
Cameron burridge <cmburridge4022@gmail.com>
2019-12-14 16:34:11 -0700

    Created Script

---
+++ Boredgame/Assets/MovingObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+
+	public Vector3 location;
+
+	void Update () 
+	{
+		transform.Translate(location);
+	}
+}
