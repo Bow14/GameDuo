e28edfa5d7616bdf5013eab3c80cf0d09dcba822
Tanner <thebow25@gmail.com>
2020-01-07 19:32:33 -0700

    Translate script for moving left and right

---
+++ Boredgame/Assets/TranslateMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TranslateMove : MonoBehaviour
+{
+	public float moveSpeed = 20f;
+
+	public float rotationSpeed;
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.Translate(Vector3.left*moveSpeed*Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			transform.Translate(Vector3.right*moveSpeed*Time.deltaTime);
+		}
+			
+	}
+}
