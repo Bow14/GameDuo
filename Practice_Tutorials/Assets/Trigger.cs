8faf482ec1a6ca7edbf326e0487a74bc8394ac8b
Tanner <thebow25@gmail.com>
2020-01-10 22:50:06 -0700

    Weird shit happened but added a following camera

---
+++ Practice_Tutorials/Assets/Trigger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Trigger : MonoBehaviour
+{
+	public UnityEvent test;
+	// Use this for initialization
+	private void OnTriggerEnter(Collider other)
+	{
+		test.Invoke();
+	}
+
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
