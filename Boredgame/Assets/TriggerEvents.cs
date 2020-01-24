21ce28798ca7e51f5e44505205a647574fcd6b29
Cameron burridge <cmburridge4022@gmail.com>
2020-01-23 17:58:31 -0700

    Added Ui and started the mouse over zoom in function for UI

---
+++ Boredgame/Assets/TriggerEvents.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+[RequireComponent(typeof(Collider))]
+public class ZoomIn : MonoBehaviour {
+
+	private void Awake()
+	{
+		GetComponent<Collider>().isTrigger = true;
+	}
+
+	public UnityEvent triggerEnterEvent, triggerExitEvent;
+	public UnityEvent triggerEvent;
+	private void OnTriggerEnter(Collider other)
+	{
+		triggerEvent.Invoke();
+		triggerEnterEvent.Invoke();
+	}
+	private void OnTriggerExit(Collider other)
+	{
+		triggerExitEvent.Invoke();
+	}
+}
---
+++ Boredgame/Assets/UITextController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.XR.WSA.Input;
+
+public class UITextController : MonoBehaviour
+{
+    public UnityEvent startEvent;
+    private Text textLabel;
+    public UnityEvent down;
+    
+    public void UpdateText(FloatData data)
+    {
+        textLabel.text = data.value.ToString();
+    }
+
+    private void Update()
+    {
+        down.Invoke();
+    }
+
+    void Awake()
+    {
+        textLabel = GetComponent<Text>();
+        startEvent.Invoke();
+    }
+
+}
